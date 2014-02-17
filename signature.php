20bc3810736898d3e7f4ade2bd8874d546c3c17d
Matias Crivolotti <matioav@gmail.com>
2014-02-17 16:40:47 +0200

    signature adjustments

--- signature.php
+++ signature.php
@@ -10,22 +10,22 @@
           <div style="margin: 0; padding: 0;">
             <span style="font-size: 18px;"><?=$_POST['name'];?></span><br />
             <span style="font-size: 13px; white-space: nowrap;"><?=$_POST['title'];?></span>,
-            <a href="http://everything.me/" style="color: #0a1116; text-decoration: none; font-size: 13px;">EverythingMe</a>
+            <a href="http://everything.me/" style="font-size: 13px; color: #0a1116; text-decoration: none;">EverythingMe</a>
           </div>
           <div style="margin: 2px 0 0 0; padding: 0; line-height: 18px; font-size: 12px; color: #737373;">
-            <span style="white-space: nowrap; margin-right: 8px;"><a href="mailto:<?=$_POST['email'];?>" style="color: #737373; text-decoration: none;"><?=$_POST['email'];?></a></span>
+            <span style="white-space: nowrap; margin-right: 8px;"><a href="mailto:<?=$_POST['email'];?>" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/mail_icon.png" border="0" valign="middle" height="16" style="height: 16px; margin-top: -1px; vertical-align: middle;"/></span>&nbsp;<span><?=$_POST['email'];?></span></a></span>
             <span style="white-space: nowrap; margin-right: 8px;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/phone_icon.png" border="0" valign="middle" height="15" style="height: 15px; vertical-align: middle;"/></span>&nbsp;<span><a href="tel:<?=$_POST['phone'];?>" style="color: #737373; text-decoration: none;"><?=$_POST['phone'];?></a></span></span>
             <?php if (!empty($_POST['additionalPhone'])): ?>
             <span style="white-space: nowrap; margin-right: 8px;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/phone_icon.png" border="0" valign="middle" height="15" style="height: 15px; vertical-align: middle;"/></span>&nbsp;<span><a href="tel:<?=$_POST['additionalPhone'];?>" style="color: #737373; text-decoration: none;"><?=$_POST['additionalPhone'];?></a></span></span>
             <?php endif; ?>
             <?php if (!empty($_POST['skype'])): ?>
-            <span style="white-space: nowrap; margin-right: 8px;"><a href="skype:<?=$_POST['skype'];?>?call" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/skype_icon.png" border="0" valign="middle" width="14" height="18" style="width: 14px; height: 18px; margin-top: -1px; vertical-align: middle;"/></span>&nbsp;<span><?=$_POST['skype'];?></span></a></span>
+            <span style="white-space: nowrap; margin-right: 8px;"><a href="skype:<?=$_POST['skype'];?>?call" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/skype_icon.png" border="0" valign="middle" height="18" style="height: 18px; margin-top: -1px; vertical-align: middle;"/></span>&nbsp;<span><?=$_POST['skype'];?></span></a></span>
             <?php endif; ?>
             <?php if (!empty($_POST['facebook'])): ?>
-            <span style="white-space: nowrap; margin-right: 8px;"><a href="http://www.facebook.com/<?=$_POST['facebook'];?>" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/facebook_icon.png" border="0" valign="middle" width="9" height="18" style="width: 9px; height: 18px; margin-top: -3px; vertical-align: middle;"/></span>&nbsp;<span><?=$_POST['facebook'];?></span></a></span>
+            <span style="white-space: nowrap; margin-right: 8px;"><a href="http://www.facebook.com/<?=$_POST['facebook'];?>" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/facebook_icon.png" border="0" valign="middle" height="18" style="height: 18px; margin-top: -3px; vertical-align: middle;"/></span>&nbsp;<span><?=$_POST['facebook'];?></span></a></span>
             <?php endif; ?>
             <?php if (!empty($_POST['twitter'])): ?>
-            <span style="white-space: nowrap; margin-right: 8px;"><a href="http://www.twitter.com/<?=$_POST['twitter'];?>" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/twitter_icon.png" border="0" valign="middle" width="16" height="18" style="width: 16px; height: 18px; margin-top: -3px; vertical-align: middle;"/></span>&nbsp;<span>@<?=$_POST['twitter'];?></span></a></span>
+            <span style="white-space: nowrap; margin-right: 8px;"><a href="http://www.twitter.com/<?=$_POST['twitter'];?>" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/twitter_icon.png" border="0" valign="middle" height="18" style="height: 18px; margin-top: -3px; vertical-align: middle;"/></span>&nbsp;<span>@<?=$_POST['twitter'];?></span></a></span>
             <?php endif; ?>
           </div>
           <?php if (!empty($_POST['extraText'])): ?>
