6af6f771e0764b41e2b7d4dfa4fc95c2898f1ea6
Matias Crivolotti <matioav@gmail.com>
2014-02-17 17:53:28 +0200

    signature width fix

--- signature.php
+++ signature.php
@@ -1,6 +1,6 @@
 <div style="font-family: Arial, sans-serif; padding-top: 13px; padding-bottom: 13px; color: #0a1116;">
   <br/>
-  <table border="0" cellpadding="0" cellspacing="0" style="width: 100%">
+  <table border="0" cellpadding="0" cellspacing="0">
     <tr>
       <td style="width: 55px;" valign="top">
         <a href="http://everything.me/"><img width="55" height="55" border="0" style="width: 55px; height: 55px;" src="<?=$img?>" /></a>
@@ -25,7 +25,7 @@
             <span style="white-space: nowrap; margin-right: 8px;"><a href="http://www.facebook.com/<?=$_POST['facebook'];?>" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/facebook_icon.png" border="0" valign="middle" height="18" style="height: 18px; margin-top: -3px; vertical-align: middle;"/></span>&nbsp;<span><?=$_POST['facebook'];?></span></a></span>
             <?php endif; ?>
             <?php if (!empty($_POST['twitter'])): ?>
-            <span style="white-space: nowrap; margin-right: 8px;"><a href="http://www.twitter.com/<?=$_POST['twitter'];?>" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/twitter_icon.png" border="0" valign="middle" height="18" style="height: 18px; margin-top: -3px; vertical-align: middle;"/></span>&nbsp;<span>@<?=$_POST['twitter'];?></span></a></span>
+            <span style="white-space: nowrap;"><a href="http://www.twitter.com/<?=$_POST['twitter'];?>" style="color: #737373; text-decoration: none;"><span><img src="https://s3.amazonaws.com/evme-static-assets/email-signature/twitter_icon.png" border="0" valign="middle" height="18" style="height: 18px; margin-top: -3px; vertical-align: middle;"/></span>&nbsp;<span>@<?=$_POST['twitter'];?></span></a></span>
             <?php endif; ?>
           </div>
           <?php if (!empty($_POST['extraText'])): ?>
